c3c9fcde97371d8299883dd5e1294a8edbb598c6
Qlas <rafgor7@gmail.com>
2020-04-08 22:29:23 +0200

    created test for item, and item worn by player. Changed test for player stats

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import unittest
 import data.player as player
 import data.map_generator as dungeon
+import data.item as item
 import numpy as np
 
 
@@ -27,12 +28,13 @@ class Player(unittest.TestCase):
 
     def test_create_character(self):
         self.assertEqual(self.name, self.character.name)
-        self.assertEqual(self.character.hp, self.character.hp_max)
-        self.assertEqual(self.character.mana, self.character.mana_max)
-        self.assertEqual(self.character.strength, 10)
-        self.assertEqual(self.character.dexterity, 7)
-        self.assertEqual(self.character.intelligence, 5)
-        self.assertEqual(self.character.defensive, 5)
+        self.assertEqual(self.character.stats['hp'], self.character.stats['max_hp'])
+        self.assertEqual(self.character.stats['mana'], self.character.stats['max_mana'])
+        self.assertEqual(self.character.stats['strength'], 10)
+        self.assertEqual(5, self.character.stats['strength'])
+        self.assertEqual(self.character.stats['dexterity'], 7)
+        self.assertEqual(self.character.stats['intelligence'], 5)
+        self.assertEqual(self.character.stats['defensive'], 5)
 
     def test_get_exp(self):
         self.assertEqual(self.character.exp, 0)
@@ -48,12 +50,45 @@ class Player(unittest.TestCase):
 
     def test_hp(self):
         self.character.take_dmg(10)
-        self.assertEqual(self.character.hp, 140)
+        self.assertEqual(self.character.stats['hp'], 140)
         self.character.heal(20)
-        self.assertEqual(self.character.hp, 150)
+        self.assertEqual(self.character.stats['hp'], 150)
         self.character.take_dmg(160)
-        self.assertEqual(self.character.hp, 1)
+        self.assertEqual(self.character.stats['hp'], 1)
         self.character.heal(200)
+
+    def test_creating_item(self):
+        player_dmg = 10
+        player_str = 5
+        stats = {'dmg': ['*', 10], 'strength': ['+', 2]}
+        items = item.Item('test', 'first_weapon', stats)
+        self.assertEqual('test', str(items))
+        first_item_bonus_dmg = items.return_bonus('dmg', player_dmg)
+        first_item_bonus_str = items.return_bonus('strength', player_str)
+        self.assertEqual(1, first_item_bonus_dmg)
+        self.assertEqual(2, first_item_bonus_str)
+        stats2 = {'dmg': ['+', 5], 'strength': ['*', 10]}
+        item2 = item.Item('test2', 'shield', stats2)
+        self.assertEqual('test2', str(item2))
+        second_item_bonus_dmg = item2.return_bonus('dmg', player_dmg)
+        second_item_bonus_str = item2.return_bonus('strength', player_str)
+        self.assertEqual(5, second_item_bonus_dmg)
+        self.assertEqual(0.5, second_item_bonus_str)
+        self.assertEqual(16, player_dmg+first_item_bonus_dmg+second_item_bonus_dmg)
+        self.assertEqual(7.5, player_str + first_item_bonus_str + second_item_bonus_str)
+
+    def test_item_in_player(self):
+        self.assertEqual(self.character.stats['hp'], self.character.stats_bonus['hp'])
+        self.assertEqual(self.character.stats['max_hp'], self.character.stats_bonus['max_hp'])
+        self.assertEqual(self.character.stats['mana'], self.character.stats_bonus['mana'])
+        self.assertEqual(self.character.stats['max_mana'], self.character.stats_bonus['max_mana'])
+        self.assertNotEqual(self.character.stats['dmg'], self.character.stats_bonus['dmg'])
+        self.assertEqual(self.character.stats['defensive'], self.character.stats_bonus['defensive'])
+        self.assertNotEqual(self.character.stats['strength'], self.character.stats_bonus['strength'])
+        self.assertEqual(self.character.stats['dexterity'], self.character.stats_bonus['dexterity'])
+        self.assertEqual(self.character.stats['intelligence'], self.character.stats_bonus['intelligence'])
+        self.assertEqual(7, self.character.stats_bonus['dmg'])
+        self.assertEqual(11, self.character.stats_bonus['strength'])
 
 
 if __name__ == '__main__':
