dd5e6f8c32931452e01dd32454626e62528367a7
Qlas <rafgor7@gmail.com>
2020-04-09 22:26:33 +0200

    change tiles*2 array to tile, and bug fixing in map generator

--- data/map_generator.py
+++ data/map_generator.py
@@ -26,17 +26,25 @@ class Room:
 class Map:
     def __init__(self, tiles):
         # map is 2 times bigger than rooms we will have
-        self.rooms = np.empty((tiles*2, tiles*2), dtype=object)
+        self.rooms = np.empty((tiles, tiles), dtype=object)
         # start room is in the middle
-        self.rooms[int(tiles)][int(tiles)] = Room('start')
+        self.rooms[int(tiles/2)][int(tiles/2)] = Room('start')
         self.tiles = tiles
         self.seed = random.randrange(sys.maxsize)
         random.seed(self.seed)
         self.build_map()
 
     def build_map(self):
+        try:
+            self.generate()
+        except IndexError:
+            self.rooms = np.empty((self.tiles, self.tiles), dtype=object)
+            self.rooms[int(self.tiles / 2)][int(self.tiles / 2)] = Room('start')
+            self.build_map()
+
+    def generate(self):
         # list of function with weight
-        function = ['monster'] * 4 + ['treasure'] * 1 + ['nothing'] * 2
+        function = ['monster'] * 4 + ['treasure'] * 1 + ['nothing'] * 0
         result = []
         while len(result) < self.tiles:
             # reshape map to 1 dimension and return not None elements
@@ -46,7 +54,7 @@ class Map:
             # get directions which is not taken for this room
             allow_direction = [key for (key, value) in room.neighbor.items() if value is None]
             if len(allow_direction) > 0:
-                # get coord of room on map (only for creating to know if room will have neighbor)
+                # get coord of room on map
                 room_coord = list(zip(np.where(self.rooms == room)[0], np.where(self.rooms == room)[1]))
                 direction = random.choice(allow_direction)
                 if len(result)+1 == self.tiles:
@@ -63,12 +71,16 @@ class Map:
 
                     # check if new room have neighbor in other dimensional
                     if self.rooms[room_coord[0][0]-1, room_coord[0][1]-1] is not None:
-                        self.rooms[room_coord[0][0] - 1, room_coord[0][1] - 1].neighbor['E'] = new_room
                         new_room.neighbor['W'] = self.rooms[room_coord[0][0] - 1, room_coord[0][1] - 1]
+                        self.rooms[room_coord[0][0] - 1, room_coord[0][1] - 1].neighbor['E'] = new_room
 
                     if self.rooms[room_coord[0][0]-1, room_coord[0][1]+1] is not None:
-                        self.rooms[room_coord[0][0] - 1, room_coord[0][1] + 1].neighbor['W'] = new_room
                         new_room.neighbor['E'] = self.rooms[room_coord[0][0] - 1, room_coord[0][1] + 1]
+                        self.rooms[room_coord[0][0] - 1, room_coord[0][1] + 1].neighbor['W'] = new_room
+
+                    if self.rooms[room_coord[0][0]-2, room_coord[0][1]] is not None:
+                        new_room.neighbor['N'] = self.rooms[room_coord[0][0] - 2, room_coord[0][1]]
+                        self.rooms[room_coord[0][0] - 2, room_coord[0][1]].neighbor['S'] = new_room
 
                 elif direction == 'S':
                     new_room = Room(actual_function)
@@ -77,12 +89,16 @@ class Map:
                     room.neighbor['S'] = new_room
 
                     if self.rooms[room_coord[0][0]+1, room_coord[0][1]-1] is not None:
-                        self.rooms[room_coord[0][0] + 1, room_coord[0][1] - 1].neighbor['E'] = new_room
                         new_room.neighbor['W'] = self.rooms[room_coord[0][0] + 1, room_coord[0][1] - 1]
+                        self.rooms[room_coord[0][0] + 1, room_coord[0][1] - 1].neighbor['E'] = new_room
 
                     if self.rooms[room_coord[0][0]+1, room_coord[0][1]+1] is not None:
-                        self.rooms[room_coord[0][0] + 1, room_coord[0][1] + 1].neighbor['W'] = new_room
                         new_room.neighbor['E'] = self.rooms[room_coord[0][0] + 1, room_coord[0][1] + 1]
+                        self.rooms[room_coord[0][0] + 1, room_coord[0][1] + 1].neighbor['W'] = new_room
+
+                    if self.rooms[room_coord[0][0]+2, room_coord[0][1]] is not None:
+                        new_room.neighbor['S'] = self.rooms[room_coord[0][0] + 2, room_coord[0][1]]
+                        self.rooms[room_coord[0][0] + 2, room_coord[0][1]].neighbor['N'] = new_room
 
                 elif direction == 'W':
                     new_room = Room(actual_function)
@@ -91,12 +107,16 @@ class Map:
                     room.neighbor['W'] = new_room
 
                     if self.rooms[room_coord[0][0]-1, room_coord[0][1]-1] is not None:
-                        self.rooms[room_coord[0][0] - 1, room_coord[0][1] - 1].neighbor['S'] = new_room
                         new_room.neighbor['N'] = self.rooms[room_coord[0][0] - 1, room_coord[0][1] - 1]
+                        self.rooms[room_coord[0][0] - 1, room_coord[0][1] - 1].neighbor['S'] = new_room
 
                     if self.rooms[room_coord[0][0]+1, room_coord[0][1]-1] is not None:
-                        self.rooms[room_coord[0][0] + 1, room_coord[0][1] - 1].neighbor['N'] = new_room
                         new_room.neighbor['S'] = self.rooms[room_coord[0][0] + 1, room_coord[0][1] - 1]
+                        self.rooms[room_coord[0][0] + 1, room_coord[0][1] - 1].neighbor['N'] = new_room
+
+                    if self.rooms[room_coord[0][0], room_coord[0][1]-2] is not None:
+                        new_room.neighbor['W'] = self.rooms[room_coord[0][0], room_coord[0][1] - 2]
+                        self.rooms[room_coord[0][0], room_coord[0][1] - 2].neighbor['E'] = new_room
 
                 elif direction == 'E':
                     new_room = Room(actual_function)
@@ -105,12 +125,16 @@ class Map:
                     room.neighbor['E'] = new_room
 
                     if self.rooms[room_coord[0][0]-1, room_coord[0][1]+1] is not None:
-                        self.rooms[room_coord[0][0] - 1, room_coord[0][1] + 1].neighbor['S'] = new_room
                         new_room.neighbor['N'] = self.rooms[room_coord[0][0] - 1, room_coord[0][1] + 1]
+                        self.rooms[room_coord[0][0] - 1, room_coord[0][1] + 1].neighbor['S'] = new_room
 
                     if self.rooms[room_coord[0][0]+1, room_coord[0][1]+1] is not None:
-                        self.rooms[room_coord[0][0] + 1, room_coord[0][1] + 1].neighbor['N'] = new_room
                         new_room.neighbor['S'] = self.rooms[room_coord[0][0] + 1, room_coord[0][1] + 1]
+                        self.rooms[room_coord[0][0] + 1, room_coord[0][1] + 1].neighbor['N'] = new_room
+
+                    if self.rooms[room_coord[0][0], room_coord[0][1]+2] is not None:
+                        new_room.neighbor['E'] = self.rooms[room_coord[0][0], room_coord[0][1] + 2]
+                        self.rooms[room_coord[0][0], room_coord[0][1] + 2].neighbor['W'] = new_room
 
                 result = np.reshape(self.rooms, -1)
                 result = [elem for elem in result if elem is not None]
