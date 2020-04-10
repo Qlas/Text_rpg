ce3dad2b2af8c8d1ab7733eacfbee5500f48a263
Qlas <rafgor7@gmail.com>
2020-04-10 21:16:41 +0200

    added requirements to items

--- data/item.py
+++ data/item.py
@@ -1,10 +1,20 @@
 class Item:
-    def __init__(self, name=None, inv_type=None, stats=None):
+    requirements = {
+            'lvl': 0,
+            'strength': 0,
+            'dexterity': 0,
+            'intelligence': 0,
+        }
+
+    def __init__(self, name=None, inv_type=None, stats=None, requirements=None):
         self.name = name
         self.inv_type = inv_type
         # stats = {'name': ['type','value']} stats ={'hp':['*',10]}
         # if *, second value is %, if we give 10, this is 10% more
         self.stats = stats
+        if requirements is not None:
+            for id_req, item in requirements.items():
+                self.requirements[id_req] = item
 
     def return_bonus(self, bonus_name, bonus_value):
         if self.name is not None and bonus_name in self.stats:
